8c5b02dd29038a522b69d2a1e2dbf68d63e0449b
A2dong <85823974+A2dong@users.noreply.github.com>
2023-11-30 13:42:53 +0000

    create todo-list

--- JS/background.js
+++ JS/background.js
@@ -1,3 +1,7 @@
-const image = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg"];
+const image = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg"];
 
 const imageChoice = image[Math.floor(Math.random()*image.length)];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${imageChoice}`;
+document.body.appendChild(bgImage);
